f5e881bce2517c7fc8927e5f275e43bbfb85bf5e
王大大 <1073125925@qq.com>
2021-01-05 19:48:18 +0800

    第二次提交

--- src/com/company/Test1.java
+++ src/com/company/Test1.java
@@ -1,11 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * @author wangdaye996
  * @createtime 2021/1/4 - 23:40
  */
 public class Test1 {
     public static void main(String[] args) {
-
+        System.out.println("请输入行和列：");
+        Scanner X=new Scanner(System.in);
+        int a=X.nextInt();
+        int b=X.nextInt();
+        for (int i = 1; i <= a; i++) {
+            for (int j=1;j<=b;j++){
+                if (i>1&&i<a&&j>1&&j<b){
+                    System.out.printf(" ");
+                }else {
+                    System.out.printf("*");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
---
+++ src/com/polymorphic/Test.java
@@ -0,0 +1,22 @@
+package com.polymorphic;
+
+import java.util.Scanner;
+
+/**
+ * @author wangdaye996
+ * @createtime 2021/1/5 - 18:18
+ */
+public class Test {
+    public static void main(String[] args) {
+        Master xiaoming=new Master();
+        System.out.println("请输入你需要领养的宠物 dog/cat:");
+        Scanner input=new Scanner(System.in);
+        String type=input.nextLine();
+        Pet pet=xiaoming.getPet(type);
+        System.out.println("你已成功领养！！");
+        System.out.println(pet.toString());
+        xiaoming.toHospital(pet);
+        System.out.println(pet.toString());
+        xiaoming.playWithPet(pet);
+    }
+}
